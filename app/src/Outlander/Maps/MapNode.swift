3e72aca258b65803863c89a7ae3befec52d10f0d
Joe McBride <joe@uicraftsman.com>
2020-06-12 17:09:39 -0700

    Initial models for maps

---
+++ app/src/Outlander/Maps/MapNode.swift
@@ -0,0 +1,117 @@
+//
+//  MapNode.swift
+//  Outlander
+//
+//  Created by Joseph McBride on 6/12/20.
+//  Copyright © 2020 Joe McBride. All rights reserved.
+//
+
+import Foundation
+
+struct MapArc {
+    var exit:String
+    var move:String
+    var destination:String
+    var hidden:Bool
+}
+
+struct MapPosition {
+    var x:Int
+    var y:Int
+    var z:Int
+}
+
+final class MapNode {
+    var id: String
+    var name: String
+    var descriptions: [String]
+    var notes: String?
+    var color: String?
+    var arcs: [MapArc]
+    var position: MapPosition
+
+    private let cardinalDirs = [
+        "north",
+        "south",
+        "east",
+        "west",
+        "northeast",
+        "northwest",
+        "southeast",
+        "southwest",
+        "out",
+        "up",
+        "down"
+    ]
+
+    init(id:String, name: String, descriptions: [String], notes: String?, color: String?, position: MapPosition, arcs: [MapArc]) {
+        self.id = id
+        self.name = name
+        self.descriptions = descriptions
+        self.notes = notes
+        self.color = color
+        self.position = position
+        self.arcs = arcs
+    }
+
+    var transferMap: String? {
+        get {
+            if self.isTransfer() {
+    //            let groups = self.notes?["(.+\\.xml)"].groups()
+    //            if groups?.count > 1 {
+    //                return groups?[1]
+    //            }
+            }
+
+            return nil
+        }
+    }
+
+    func isTransfer() -> Bool {
+        self.notes?.contains(".xml") == true
+    }
+
+    func arcWith(id: String) -> MapArc? {
+        return arcs.filter { $0.destination == id }.first
+    }
+
+    func nonCardinalExists() -> [MapArc] {
+        return arcs.filter { !self.cardinalDirs.contains($0.exit) }
+    }
+
+    func cardinalExits() -> [String] {
+        return arcs.filter { self.cardinalDirs.contains($0.exit) }.map { $0.exit }.sorted()
+    }
+
+    func matchesExits(_ exits: [String]) -> Bool {
+        return self.cardinalExits().elementsEqual(exits, by: {
+            $0 == $1
+        })
+    }
+
+    func matches(name: String, description: String, exits: [String], ignoreTransfers: Bool) -> Bool {
+
+        if (ignoreTransfers && self.isTransfer()) {
+            return false
+        }
+
+        if exits.count > 0 {
+            return self.matchesExits(exits)
+                && self.name == name
+                && self.hasMatchingDescription(description)
+        }
+
+        return self.name == name && self.hasMatchingDescription(description)
+    }
+
+    func hasMatchingDescription(_ description: String) -> Bool {
+        let mod = description.replacingOccurrences(of: "\"", with: "").replacingOccurrences(of: ";", with: "")
+
+        for desc in descriptions {
+            if desc.hasPrefix(mod) {
+                return true
+            }
+        }
+        return false
+    }
+}
